63240bad0f723cc9698988fb4e1cd007cc07a702
ARAVIND9377 <aravindsak62@gmail.com>
2025-10-23 18:24:30 +0530

    Upadated product search functionality

--- products.js
+++ products.js
@@ -4,10 +4,33 @@ var sidenav = document.getElementById("sidenav")
 var menuicon = document.getElementById("menuicon")
 var closenav = document.getElementById("closenav")
 
-menuicon.addEventListener("click",function(){
-    sidenav.style.right=0;
+menuicon.addEventListener("click", function () {
+    sidenav.style.right = 0;
 })
 
-closenav.addEventListener("click",function(){
-    sidenav.style.right="-50%"
+closenav.addEventListener("click", function () {
+    sidenav.style.right = "-50%"
+})
+
+// Product Functionality
+
+var productCotainer = document.getElementById("product-container")
+var search = document.getElementById("search")
+var productList = productCotainer.querySelectorAll("div")
+
+
+
+search.addEventListener("keyup", function () {
+    var enterdvalue = event.target.value.toUpperCase()
+
+    for (count = 0; count < productList.length; count = count + 1) {
+
+        var productname = productList[count].querySelectorAll("h1").textContent
+        if (productname[count].textContent.toUpperCase().indexOf(enterdvalue) < 0) {
+            productList[count].style.display = "none"
+        } else {
+            productList[count].style.display = "block"
+        }
+
+    }
 })
